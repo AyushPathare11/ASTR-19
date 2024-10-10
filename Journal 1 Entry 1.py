699a4e2b04f438a89cf1cb890e8df69712cad96e
AyushPathare11 <apathare@ucsc.edu>
2024-10-09 17:40:20 -0700

    Section one prompt for Journal one

---
+++ Journal 1 Entry 1.py
@@ -0,0 +1,5 @@
+print("Hi, My name is Ayush Atul Pathare (He/Him) and I am a sophmore at UCSC ")
+print("My fav movie is Johnny English which is a comedy movie which ligthens my mood everytime I watch it")
+print("Its about the fall of England and how our protagonist saves it. (with comedic twists of course)")
+print("my fav food is a a chicken burger just because it tastes so nice and easy to make. ")
+print("it might not be gourmet food but its alwasy pleasant to eat")
