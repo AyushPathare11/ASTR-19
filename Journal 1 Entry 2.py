b9435960d98979dde186c6550cd18a814a00eb60
AyushPathare11 <apathare@ucsc.edu>
2024-10-09 17:52:10 -0700

    Section 2 of journal 1

---
+++ Journal 1 Entry 2.py
@@ -0,0 +1,25 @@
+def sum_of_floats():
+    x = 2.3
+    y = 3.14
+    print(x+y)
+    print(type(x+y))
+
+def diff_of_ints():
+    x = 7
+    y = -25
+    print(x - y)
+    print(type(x - y))
+
+def products_of_comb():
+    x = 27
+    y = 3.5
+    print(x*y)
+    print(type(x * y))
+
+def main():
+    sum_of_floats()
+    diff_of_ints()
+    products_of_comb()
+
+if __name__ =="__main__":
+    main()
