23cd39b2ccaddfeae5ace39f9d5ffd246ff4c4cc
AyushPathare11 <apathare@ucsc.edu>
2024-10-09 17:43:28 -0700

    Journal one for prompt one (real)

--- Journal 1 Entry 1.py
+++ Journal 1 Entry 1.py
@@ -1,5 +1,12 @@
-print("Hi, My name is Ayush Atul Pathare (He/Him) and I am a sophmore at UCSC ")
-print("My fav movie is Johnny English which is a comedy movie which ligthens my mood everytime I watch it")
-print("Its about the fall of England and how our protagonist saves it. (with comedic twists of course)")
-print("my fav food is a a chicken burger just because it tastes so nice and easy to make. ")
-print("it might not be gourmet food but its alwasy pleasant to eat")
+def printAyush():
+    print("Hi, My name is Ayush Atul Pathare (He/Him) and I am a sophmore at UCSC ")
+    print("My fav movie is Johnny English which is a comedy movie which ligthens my mood everytime I watch it")
+    print("Its about the fall of England and how our protagonist saves it. (with comedic twists of course)")
+    print("my fav food is a a chicken burger just because it tastes so nice and easy to make. ")
+    print("it might not be gourmet food but its alwasy pleasant to eat")
+
+def main():
+    printAyush()
+
+if __name__ =="__main__":
+    main()
